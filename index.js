25d601d738058d765cec92cbf81750873d0609d7
Mohammad Asad Mohammad <m91.alahmadi@gmail.com>
2016-05-29 14:26:18 -0700

    deploy

--- index.js
+++ index.js
@@ -5,30 +5,46 @@ var https = require('https')
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session')
+var morgan = require('morgan')
 
 var api = require('./api.js')
 
-
 var app = express();
-
+var FileStore = require('session-file-store')(session);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
+app.use(morgan('dev'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-
 // Config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// session configuration
+app.use(session({
+  name: 'server-session-cookie-id',
+  secret: process.env.SECRET,
+  saveUninitialized: true,
+  cookie: { httpOnly: true, 
+            secure: false, 
+            maxAge: null,},
+  store: new FileStore(),
+  resave: false,
+}))
+
+
+
 
 // index: main view 
 app.get('/', function(request, response) {
-  response.render('pages/index', {redirect_uri: '#nothing'});
+  if (req.session.token) {
+    console.log('already logged in')
+  }
+  response.render('pages/index');  
 });
 
 
@@ -53,6 +69,7 @@ app.get('/callback', function(req, res){
         console.log(err)
       } else {
         console.log(json)
+        req.session.token = json.access_token;
       };
       res.send('authentication sucess!');
     })
@@ -62,6 +79,6 @@ app.get('/callback', function(req, res){
 
 
 // run app
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), '0.0.0.0', function() {
   console.log('Node app is running on port', app.get('port'));
 });
