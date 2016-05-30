dc9e59fe5351a666c08d389d9893debf54d85ad1
Mohammad Asad Mohammad <m91.alahmadi@gmail.com>
2016-05-30 16:21:14 -0700

    deploy

--- api.js
+++ api.js
@@ -1,17 +1,32 @@
 
 var request = require('request');
 
+var headers = function(token) {
+  return {
+    'X-Access-Token' : token,
+    'X-Client-ID' : process.env.CLIENT_ID
+  }
+}
+
 module.exports = {
 
   getToken: function getToken(code, callback) {
     request.post({
-      url: 'https://www.wunderlist.com/oauth/access_token',
-      json: {
+      url : 'https://www.wunderlist.com/oauth/access_token',
+      json : {
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
         code: code,
       }
     }, callback);
+  },
+
+  createList: function createList(token, title, callback) {
+    request.post({
+      url : 'https://a.wunderlist.com/api/v1/lists',
+      headers : headers(token),
+      json : { title : title },
+      }, callback)
   }
 
 }
--- index.js
+++ index.js
@@ -12,7 +12,6 @@ var api = require('./api.js')
 
 var app = express();
 
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -38,6 +37,24 @@ app.use(session({
 }))
 
 
+// for message flashing
+var flash = function(type, text) {
+  var classes = (type === 'success' ? 'chip green lighten-4 green-text text-darken-1' : 'chip red lighten-4 red-text text-lighten-1')
+  return {
+    classes: classes,
+    text: text + '<i class="material-icons">close</i>'}
+};
+// Session-persisted flash message middleware
+app.use(function(req, res, next){
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = flash('error', err);
+  if (msg) res.locals.message = flash('success', msg);
+  next();
+});
 
 
 // index: main view 
@@ -80,16 +97,29 @@ app.get('/callback', function(req, res){
         console.log(json)
         req.session.token = json.access_token;
       };
-      res.redirect('/parse-tasks');
+      res.redirect('/parser');
     })
     
   }
 })
 
 
-// test page 
-app.get('/parse-tasks', function(req, res){
-  res.render('pages/parse-tasks')
+// parser page
+app.get('/parser', function(req, res){
+  if (req.query)
+  res.render('pages/parser')
+})
+
+app.post('/parser', function(req, res){
+  var title = req.body.title
+  api.createList(req.session.token, title, function(err, apires, json){
+    if (!error && response.statusCode == 201) {
+      req.session.success = `<b>${title}</b> list created...`
+    }
+    console.log(json)
+  })
+  req.session.error = 'omg omg succeeded'
+  res.redirect('/parser')
 })
 
 // run app
